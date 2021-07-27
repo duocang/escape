81b750d872de342dff20143978220d8f26e79ef6
duocang <wangxuesong29@gmail.com>
2021-07-27 16:44:50 +0200

    add assay and slot options for Seurat case

--- R/enrich.R
+++ R/enrich.R
@@ -1,18 +1,20 @@
 #' Calculate gene set enrichment scores for single-cell data
 #'
-#' This function allows users to input both the single-cell RNA-sequencing 
-#' counts and any gene set pathways either from the stored data or from 
-#' other sources. The enrichment calculation itself 
+#' This function allows users to input both the single-cell RNA-sequencing
+#' counts and any gene set pathways either from the stored data or from
+#' other sources. The enrichment calculation itself
 #' uses the gsva R package and the poisson distribution for RNA.
 #'
 #' @param obj The count matrix, Seurat, or SingleCellExperiment object.
-#' @param gene.sets Gene sets from \code{\link{getGeneSets}} to use 
+#' @param gene.sets Gene sets from \code{\link{getGeneSets}} to use
 #' for the enrichment analysis. Alternatively a simple base R list where
 #' the names of the list elements correspond to the name of the gene set
 #' and the elements themselves are simple vectors of gene names representing
-#' the gene set. 
+#' the gene set.
 #' @param groups The number of cells to separate the enrichment calculation.
 #' @param cores The number of cores to use for parallelization.
+#' @param assay The assay for Seuart object, only used when obj ist a Seruat object
+#' @param slot The slot for Seuart object, only used when obj ist a Seruat object
 #'
 #' @importFrom GSVA gsva
 #' @importFrom GSEABase GeneSetCollection geneIds
@@ -20,14 +22,14 @@
 #' @importFrom BiocParallel SnowParam
 #' @importFrom Matrix summary
 #'
-#' 
-#' @examples 
+#'
+#' @examples
 #' # download HALLMARK gene set collection
-#' GS <- getGeneSets(library = "H") 
+#' GS <- getGeneSets(library = "H")
 #' GS <- GS[c(1:2)] #Reduce list size for example
 #' seurat_ex <- suppressWarnings(SeuratObject::pbmc_small)
 #' ES <- enrichIt(obj = seurat_ex, gene.sets = GS)
-#' 
+#'
 #' # alternatively, construct your own list of gene sets
 #' myGS <- list(Housekeeping = c("ACTA1", "ACTN1", "GAPDH"),
 #'   Cancer = c("TP53","BRCA2","ERBB2","MYC"))
@@ -37,17 +39,22 @@
 #'
 #' @seealso \code{\link{getGeneSets}} to collect gene sets.
 #' @return Data frame of normalized enrichmenet scores (NES)
-enrichIt <- function(obj, gene.sets = NULL, groups = 1000, cores = 2) {
-    
+enrichIt <- function(obj, gene.sets = NULL, groups = 1000, cores = 2, assay = "RNA", slot = "counts") {
+
     if(is.null(gene.sets)) {
-        stop("Please provide the gene.sets you would like to use for 
+        stop("Please provide the gene.sets you would like to use for
             the enrichment analysis")
     } else {
         egc <- gene.sets
     }
-    
+
     if (inherits(x = obj, what = "Seurat")) {
-        cnts <- obj@assays[["RNA"]]@counts
+        if (slot == "counts") {
+            cnts <- obj@assays[[slot]]@counts
+        }
+        if (slot == "data"){
+            cnts <- obj@assays[[slot]]@data
+        }
         cnts<- cnts[tabulate(summary(cnts)$i) != 0, , drop = FALSE]
         cnts <- as.matrix(cnts)
     } else if (inherits(x = obj, what = "SingleCellExperiment")) {
@@ -58,26 +65,26 @@ enrichIt <- function(obj, gene.sets = NULL, groups = 1000, cores = 2) {
         cnts <- obj
     }
     if(inherits(egc, what = "GeneSetCollection")){
-        egc <- GSEABase::geneIds(egc) # will return a simple list, 
+        egc <- GSEABase::geneIds(egc) # will return a simple list,
         #which will work if a matrix is supplied to GSVA
     }
-    cnts <- cnts[rowSums(cnts > 0) != 0, ] 
+    cnts <- cnts[rowSums(cnts > 0) != 0, ]
     # break to groups of cells
     scores <- list()
     wind <- seq(1, ncol(cnts), by=groups)
-    print(paste('Using sets of', groups, 'cells. Running', 
+    print(paste('Using sets of', groups, 'cells. Running',
             length(wind), 'times.'))
-    
+
     for (i in wind) {
         last <- min(ncol(cnts), i+groups-1)
-        a <- suppressWarnings(gsva(cnts[,i:last], egc, method = 'ssgsea', 
-            ssgsea.norm = TRUE, kcdf = "Poisson", parallel.sz = cores, 
+        a <- suppressWarnings(gsva(cnts[,i:last], egc, method = 'ssgsea',
+            ssgsea.norm = TRUE, kcdf = "Poisson", parallel.sz = cores,
             BPPARAM = SnowParam()))
         scores[[i]] <- a
     }
-    
+
     scores <- do.call(cbind, scores)
     output <- data.frame(t(scores))
-    
+
     return(output)
 }
